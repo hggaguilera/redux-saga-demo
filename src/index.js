40ac4d1ff4dcf79b00a21c0ffd1cbc3d559cc8bd
Gabriel Gonzalez <hgglez92@gmail.com>
2020-08-17 14:10:19 -0600

    initial setup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import axios from "axios";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = "http://rem-rest-api.herokuapp.com/api";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
